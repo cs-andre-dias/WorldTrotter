4c77726a1cf1267f15a77bbaefda7985d11ce4b3
Andre Dias <andre.dias@concrete.com.br>
2016-10-07 11:00:55 -0300

    Adiciona metodo de conversao de fahrenheit para celsius, metodo para limitar a 1 digito apos o ponto na decimal

--- WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -10,8 +10,55 @@ import UIKit
 
 class ConversionViewController: UIViewController {
     
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    var fahrenheitValue: Double? {
+        didSet{
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Double? {
+        if let value = fahrenheitValue{
+            return (value - 32) * (5/9)
+        }else{
+            return nil
+        }
+    }
+    
+    let numberFormatter = { () -> NumberFormatter in
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }
+    
+    func updateCelsiusLabel(){
+        if let value = celsiusValue {
+            celsiusLabel.text = numberFormatter().string(from: value as NSNumber)
+        }else{
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    @IBAction func fahrenheitFieldEditChanged(textField: UITextField){
+        if let text = textField.text {
+            if let value = Double(text) {
+                fahrenheitValue = value
+            }else{
+                celsiusLabel.text = "???"
+            }
+        }
+    }
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    
+    @IBAction func dismissKeyboard(sender: AnyObject){
+        textField.resignFirstResponder()
+    }
+    
 }
